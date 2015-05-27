8263442ebd7e9e853ccfa8472e810cbcd68b8bfa
xavi016 <xaviernavarro_1994@hotmail.com>
2015-05-27 16:30:35 +0200

    controllersv3

--- app/controllers/RatingController.php
+++ app/controllers/RatingController.php
@@ -5,11 +5,20 @@ class RatingController extends BaseController {
 		public function rating2()
 		{
 			
-		
-			$acciones = DB::table('acciones')->select('id', 'desc')->get();
-			$dietas = DB::table('dietas')->select('id', 'desc')->get();
-			//return View::make('pages.rating', array('acciones' => $acciones));
-			return json_encode(array('acciones' => $acciones));
-			return json_encode(array('dietas' => $dietas));
+			$id = $_GET['id'];
+
+			if ($id!=null){
+				$acciones = DB::table('acciones')->where('id', $id)->get();
+				//$acciones = DB::table('acciones')->where('id', $id)->pluck('id');
+
+				$dietas = DB::table('dietas')->select('id', 'desc')->get();
+				//return View::make('pages.rating', array('acciones' => $acciones));
+				return json_encode(array('acciones' => $acciones));
+				//return json_encode(array('dietas' => $dietas));
+			}else{
+				$acciones = DB::table('acciones')->select('*')->get();
+				return json_encode(array('acciones' => $acciones));
+			}
+			
 		}
   }
